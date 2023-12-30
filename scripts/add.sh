e33b52c7e6e9df5cfb47bd688aba673e55b125ae
zaky <ahmed6mohamed6@gmail.com>
2023-12-30 21:12:35 +0200

    add github workflows + scripts

---
+++ scripts/add.sh
@@ -0,0 +1,17 @@
+
+add() {
+
+    local NUM1=$1
+    local NUM2=$2
+
+    SUM=$((NUM1 + NUM2))
+}
+
+IFS=$'\n' read -r -d '' COUNT < count.txt
+
+NUM1=$COUNT
+NUM2=$1
+
+sub $NUM1 $NUM2
+
+echo -n "$SUM" > count.txt
---
+++ scripts/sub.sh
@@ -0,0 +1,20 @@
+sub() {
+
+    local NUM1=$1
+    local NUM2=$2
+    
+    if (( NUM2 > NUM1 )); then
+        SUM=0
+    fi
+
+    SUM=$((NUM1 - NUM2))
+}
+
+IFS=$'\n' read -r -d '' COUNT < count.txt
+
+NUM1=$COUNT
+NUM2=$1
+
+sub $NUM1 $NUM2
+
+echo -n "$SUM" > count.txt
